bd4a749601b017bbf750c28de30af6662d092bf2
Brandon V <brandon15811@gmail.com>
2017-01-10 11:42:43 -0600

    Hopefully fix OAuth redirection

--- app.py
+++ app.py
@@ -66,6 +66,34 @@ class FlaskExt(flask.Flask):
         super(FlaskExt, self).__init__(resource_name)
         self._hook_routes()
 
+# Based off of http://flask.pocoo.org/snippets/35/
+class ReverseProxied(object):
+    '''Wrap the application in this middleware and configure the
+    front-end server to add these headers, to let you quietly bind
+    this to a to an HTTP scheme that is different than what is used
+    locally.
+
+    In nginx:
+    location /myprefix {
+        proxy_pass http://192.168.0.1:5001;
+        proxy_set_header Host $host;
+        proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+        proxy_set_header X-Scheme $scheme;
+        }
+
+    :param app: the WSGI application
+    '''
+    def __init__(self, app):
+        self.app = app
+
+    def __call__(self, environ, start_response):
+        scheme = environ.get('HTTP_X_SCHEME', '')
+        if scheme:
+            environ['wsgi.url_scheme'] = scheme
+        host = environ.get('HTTP_X_FORWARDED_SERVER', '')
+        if host:
+            environ['HTTP_HOST'] = host
+        return self.app(environ, start_response)
 
 class WebServer(FlaskExt):
     """Wrapper for web-server functionality"""
@@ -89,6 +117,7 @@ class WebServer(FlaskExt):
     @staticmethod
     def start(wsgi_mode):
         app = WebServer()
+        app.wsgi_app = ReverseProxied(app.wsgi_app)
         if not wsgi_mode:
             app.run(host='::', port=int(os.environ.get('PORT', 8080)),
                     ssl_context=('fullchain.pem', 'privkey.pem'), debug=True)
